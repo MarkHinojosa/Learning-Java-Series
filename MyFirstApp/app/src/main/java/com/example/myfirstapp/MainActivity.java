a23a88494210c3c9c54af9f7d8db39412c875369
mark <markhinojosa1212@gmail.com>
2019-05-06 19:52:29 -0500

    adding android studio project

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myfirstapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
+    private TextView showText;
+    private EditText enterName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button);
+        enterName = findViewById(R.id.editText);
+        myButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                String name = enterName.getText().toString();
+
+                showText.setText("Hello " + name );
+            }
+        });
+
+        showText = findViewById(R.id.textView);
+
+
+    }
+}
