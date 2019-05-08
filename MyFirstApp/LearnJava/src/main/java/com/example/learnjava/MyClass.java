e34e68f36ffab39f588e8bc0e79e0585e24307e4
mark <markhinojosa1212@gmail.com>
2019-05-07 22:06:05 -0500

    integers and concatenation

--- MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -4,5 +4,11 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
+
+        //variable == bucket in memory
+        int age = 23;
+        String name = "Georgina";
+
+        System.out.println("My name is " + name + " and I'm " + age + " years old.");
     }
 }
