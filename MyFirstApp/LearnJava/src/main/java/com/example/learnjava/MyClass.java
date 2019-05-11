932ed42ef85d1e2c94e47a96645f68132c1f3023
mark <markhinojosa1212@gmail.com>
2019-05-10 23:22:59 -0500

    video 11. Long, Floats and Doubles

--- MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -7,8 +7,19 @@ public class MyClass {
 
         //variable == bucket in memory
         int age = 23;
+        //int is smaller in capacity than long
+        long weightOfPlanet = 944342543;
+
+        //1.3,4.4, 0.2... decimal numbers
+
+        //add f to signify float or double
+        float pi = 3.14f;
+        double pid = 3.14; //bigger with more precision
+
+
+
         String name = "Georgina";
 
-        System.out.println("My name is " + name + " and I'm " + age + " years old.");
+        System.out.println("My name is " + pid  + " and I'm " + age + " years old.");
     }
 }
