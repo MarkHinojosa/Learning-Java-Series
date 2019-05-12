ae90e601fbd75464845a14cc7dc49ebe89c334c6
mark <markhinojosa1212@gmail.com>
2019-05-11 22:38:02 -0500

    Char,Byte and Short Types in Java

--- MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -6,9 +6,12 @@ public class MyClass {
         System.out.println("Hello World");
 
         //variable == bucket in memory
-        int age = 23;
-        //int is smaller in capacity than long
+        int age = 23; //int is smaller in capacity than long
         long weightOfPlanet = 944342543;
+        byte b = 127; // 127 is max
+        short s = 156; //shot is 2x size of byte
+
+
 
         //1.3,4.4, 0.2... decimal numbers
 
@@ -18,7 +21,8 @@ public class MyClass {
 
 
 
-        String name = "Georgina";
+        String name = "Georgina";//unlimited string length
+        char a = 77; //only 1 character allowed, or number converted to unicode, notice single quotes
 
         System.out.println("My name is " + pid  + " and I'm " + age + " years old.");
     }
