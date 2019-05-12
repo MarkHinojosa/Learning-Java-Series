cc10b4b21227dd9a27ed2eca738563695ab7f63a
mark <markhinojosa1212@gmail.com>
2019-05-11 23:03:40 -0500

    Add, Div, Mult, Sub

--- MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -2,32 +2,41 @@ package com.example.learnjava;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
-        //variable == bucket in memory
-        int age = 23; //int is smaller in capacity than long
-        long weightOfPlanet = 944342543;
-        byte b = 127; // 127 is max
-        short s = 156; //shot is 2x size of byte
 
 
 
-        //1.3,4.4, 0.2... decimal numbers
+//        System.out.println("Hello World");
 
-        //add f to signify float or double
-        float pi = 3.14f;
-        double pid = 3.14; //bigger with more precision
+//        //variable == bucket in memory
+//        int age = 23; //int is smaller in capacity than long
+//        long weightOfPlanet = 944342543;
+//        byte b = 127; // 127 is max
+//        short s = 156; //shot is 2x size of byte
+//
+//
+//
+//        //1.3,4.4, 0.2... decimal numbers
+//
+//        //add f to signify float or double
+//        float pi = 3.14f;
+//        double pid = 3.14; //bigger with more precision
+//
+//
+//
+//        String name = "Georgina";//unlimited string length
+//        char a = 77; //only 1 character allowed, or number converted to unicode, notice single quotes
+//
+//
+//        boolean isTrue;
+//        isTrue = true;
+//
+//        System.out.println("My name is " + isTrue  + " and I'm " + age + " years old.");
 
+        double a = 12;
+        double b = 12;
 
-
-        String name = "Georgina";//unlimited string length
-        char a = 77; //only 1 character allowed, or number converted to unicode, notice single quotes
-
-
-        boolean isTrue;
-        isTrue = true;
-
-        System.out.println("My name is " + isTrue  + " and I'm " + age + " years old.");
+        //Subtraction and addition (-,+)
+        double total = (int) (b / a);//casting
+        System.out.println("The total is " + total);
     }
 }
