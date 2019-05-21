6e6063d07fa6e8fc7e10a276b14bf9f862c4b13e
mark <markhinojosa1212@gmail.com>
2019-05-21 00:46:36 -0500

    video 20 - For Loops

--- MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ MyFirstApp/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -81,7 +81,17 @@ public class MyClass {
 
          */
 
+       /*
+            - Video #20 -
+            ===For Loops===
+         for (int i = 0; i < 20; i++) {
+            if( i % 3 == 0){
+                System.out.println( i + " is multiple of 3");
+            }
+        }
+        */
 
-    
+
+
     }
 }
