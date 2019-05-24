ae23a07221cd013f28d00216426e313499d420bb
mark <markhinojosa1212@gmail.com>
2019-05-24 01:34:31 -0500

    Video 28 - Creating First Java Class

--- MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/MyClass.java
+++ MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/MyClass.java
@@ -2,4 +2,32 @@ package com.example.introtooopinjava;
 
 public class MyClass {
     // ____Intro To Object Oriented Programming In Java___
+    public static void main(String[] args){
+
+        /*
+        --- Video 28 ---
+        ===Creating First Java Class
+        Microphone microphone = new Microphone();
+        microphone.color = "Blue";
+        microphone.name = "Blue Yeti";
+        microphone.model = 1234;
+
+        Microphone rode = new Microphone();
+        microphone.color = "Red";
+        microphone.name = "Rode";
+        microphone.model = 12344;
+
+        System.out.println(rode);
+        System.out.println(microphone);
+
+        Shoes shoes = new Shoes();
+        shoes.color = "yellow";
+
+        System.out.println(shoes);
+
+                 */
+
+
+    }
+
 }
---
+++ MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/Shoes.java
@@ -0,0 +1,6 @@
+package com.example.introtooopinjava;
+
+public class Shoes {
+
+    String color;
+}
