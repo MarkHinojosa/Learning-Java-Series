afe67be8d6ae50bc6e7b75f3ac60f141c541ec97
mark <markhinojosa1212@gmail.com>
2019-05-25 10:39:33 -0500

    Video 29 - Adding the Description Method to the Class

--- MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/Microphone.java
+++ MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/Microphone.java
@@ -4,4 +4,20 @@ public class Microphone {
     String name;
     String color;
     int model;
+
+
+    public void  turnOn(){
+        System.out.println(this.name + " Turn On");
+    }
+    public void turnOff () {
+        System.out.println(this.name + " Turn Off");
+
+    }
+    public void setVolume(){
+        System.out.println(this.name + " Setting Volume");
+    }
+
+    public String showDescription(){
+        return "Name:" + this.name + " Color: " + this.color + " Model: " + this.model;
+    }
 }
