f7837193bd6dfdb08c8c3fb6389cc1d96b8a9969
mark <markhinojosa1212@gmail.com>
2019-06-04 11:30:09 -0500

    Video 37 - Overriding Super Class Method

--- MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/Manager.java
+++ MyFirstApp/IntroToOOPInJava/src/main/java/com/example/introtooopinjava/Manager.java
@@ -2,4 +2,13 @@ package com.example.introtooopinjava;
 
 public class Manager extends Employee {
 
+
+        /*
+        ---Video 37 ---
+        ===Overriding Super Class Method===
+         */
+    @Override
+    public double getAnnualSalary() {
+        return super.getAnnualSalary() + 10000;
+    }
 }
